947e1fd1367b5bd9891738319acb6c0c66c05a44
Tish254 <tiegomseeraoscar295@gmail.com>
2023-04-19 00:07:01 +0300

    dev: finished the top part of the homescreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,20 +1,41 @@
-import { SafeAreaView, StyleSheet, Text, StatusBar, View } from 'react-native'
-import React from 'react'
-
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  StatusBar,
+  View,
+  Image,
+} from "react-native";
+import React from "react";
+import tw from "twrnc";
+import NavOptions from "../components/NavOptions";
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>I am the Homescreen</Text>
-    </SafeAreaView>
-  )
-}
+    <SafeAreaView style={[tw`bg-white h-full`, styles.container]}>
+      <View style={tw`p-5`}>
+        <Image
+          style={{
+            width: 100,
+            height: 100,
+            resizeMode: "contain",
+          }}
+          source={{
+            uri: "https://links.papareact.com/gzs",
+          }}
+        />
 
-export default HomeScreen
+        <NavOptions />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: StatusBar.currentHeight
-},
-})
+    marginTop: StatusBar.currentHeight,
+  },
+});
